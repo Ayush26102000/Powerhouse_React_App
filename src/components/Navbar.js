8c9776152070e10a1e86e2b5e700abb4558ad6c1
Ayush Kamble <aykkamble098@gmail.com>
2022-12-24 23:36:51 +0530

    Root problem solved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
-import React, { useState} from 'react'
+import React, { useState} from 'react';
+// import logo from 'fontawesome/';
 
 const navbar = () => {
   return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                
+                <link to="/" className='navbar-logo'></link>
             </div>
         </nav>
     </>
