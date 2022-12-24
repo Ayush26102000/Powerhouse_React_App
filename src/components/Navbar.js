11bada1181e7e739ce4fe75e680d0593292d2e02
Ayush Kamble <aykkamble098@gmail.com>
2022-12-24 21:55:25 +0530

    start of navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { useState} from 'react'
+
+const navbar = () => {
+  return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default navbar
